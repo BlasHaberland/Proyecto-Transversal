f137e36cdf1c0e45df3c103ea55c726410ee2d40
Federico González <alambratore@hotmail.com>
2024-05-14 16:02:04 -0300

    alumnoData: Buscar por dni, modificar, eliminar

--- src/main/java/Entidades/AlumnoData.java
+++ src/main/java/Entidades/AlumnoData.java
@@ -8,80 +8,158 @@ import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class AlumnoData {
 
-    private final Connection connection;
+  private final Connection connection;
 
-    public AlumnoData() {
-        DBConnection instance = DBConnection.getInstance();
-        this.connection = instance.getConnection();
+  public AlumnoData() {
+    DBConnection instance = DBConnection.getInstance();
+    this.connection = instance.getConnection();
+  }
+
+  public List<Alumno> listarAlumnos() {
+    List<Alumno> alumnos = new ArrayList<>();
+    try {
+      String sql = "SELECT * FROM alumnos WHERE estado = 1;";
+      Statement st = this.connection.createStatement();
+      ResultSet rs = st.executeQuery(sql);
+
+      while (rs.next()) {
+        int idAlumno = rs.getInt("id_Alumno");
+        int dni = rs.getInt("dni");
+        String apellido = rs.getString("apellido");
+        String nombre = rs.getString("nombre");
+        LocalDate fechaNac = rs.getDate("fecha_Nacimiento").toLocalDate();
+
+        Alumno alumno = new Alumno(idAlumno, dni, apellido, nombre, fechaNac, true);
+
+        alumnos.add(alumno);
+      }
+
+      st.close();
+    } catch (SQLException ex) {
+      System.err.println("Error en SQL");
+      System.out.println(ex);
     }
+    return alumnos;
+  }
 
-    public List<Alumno> listarAlumnos() {
-        List<Alumno> alumnos = new ArrayList<>();
-        try {
-            String sql = "SELECT * FROM alumnos WHERE estado = 1;";
-            Statement st = this.connection.createStatement();
-            ResultSet rs = st.executeQuery(sql);
+  public Alumno buscarAlumno(int id) {
+    Alumno alumno = null;
 
-            while (rs.next()) {
-                int idAlumno = rs.getInt("id_Alumno");
-                int dni = rs.getInt("dni");
-                String apellido = rs.getString("apellido");
-                String nombre = rs.getString("nombre");
-                LocalDate fechaNac = rs.getDate("fecha_Nacimiento").toLocalDate();
+    try {
+      String sql = "SELECT * FROM alumnos WHERE estado = 1 AND id_Alumno = ?";
+      PreparedStatement ps = connection.prepareStatement(sql);
+      ps.setInt(1, id);
+      ResultSet rs = ps.executeQuery();
 
-                Alumno alumno = new Alumno(idAlumno, dni, apellido, nombre, fechaNac, true);
+      while (rs.next()) {
+        int idAlumno = rs.getInt("id_Alumno");
+        int dni = rs.getInt("dni");
+        String apellido = rs.getString("apellido");
+        String nombre = rs.getString("nombre");
+        LocalDate fechaNac = rs.getDate("fecha_Nacimiento").toLocalDate();
 
-                alumnos.add(alumno);
-            }
+        alumno = new Alumno(idAlumno, dni, apellido, nombre, fechaNac, true);
+      }
 
-        } catch (SQLException ex) {
-            System.err.println("Error en SQL");
-        }
-        return alumnos;
+      ps.close();
+    } catch (SQLException ex) {
+      System.err.println("Error en SQL");
+      System.out.println(ex);
     }
+    return alumno;
+  }
 
-    public Alumno buscarAlumno(int id) {
-        Alumno alumno = null;
+  public Alumno buscarAlumnoPorDni(int document) {
+    Alumno alumno = null;
 
-        try {
-            String sql = "SELECT * FROM alumnos WHERE estado = 1 AND id_Alumno = ?";
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ps.setInt(1, id);
-            ResultSet rs = ps.executeQuery();
+    try {
+      String sql = "SELECT * FROM alumnos WHERE estado = 1 AND dni = ?";
+      PreparedStatement ps = connection.prepareStatement(sql);
+      ps.setInt(1, document);
+      ResultSet rs = ps.executeQuery();
 
-            while (rs.next()) {
-                int idAlumno = rs.getInt("id_Alumno");
-                int dni = rs.getInt("dni");
-                String apellido = rs.getString("apellido");
-                String nombre = rs.getString("nombre");
-                LocalDate fechaNac = rs.getDate("fecha_Nacimiento").toLocalDate();
+      while (rs.next()) {
+        int idAlumno = rs.getInt("id_Alumno");
+        int dni = rs.getInt("dni");
+        String apellido = rs.getString("apellido");
+        String nombre = rs.getString("nombre");
+        LocalDate fechaNac = rs.getDate("fecha_Nacimiento").toLocalDate();
 
-                alumno = new Alumno(idAlumno, dni, apellido, nombre, fechaNac, true);
-            }
-        } catch (SQLException ex) {
-            System.err.println("Error en SQL");
-        }
-        return alumno;
+        alumno = new Alumno(idAlumno, dni, apellido, nombre, fechaNac, true);
+      }
+
+      ps.close();
+    } catch (SQLException ex) {
+      System.err.println("Error en SQL");
     }
+    return alumno;
+  }
 
-    public void guardarAlumno(Alumno alumno) {
-        try {
-            String sql = "INSERT INTO alumnos(dni,apellido,nombre,fecha_Nacimiento,estado) VALUES(?,?,?,?,?)";
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ps.setInt(1, alumno.getDni());
-            ps.setString(2, alumno.getApellido());
-            ps.setString(3, alumno.getNombre());
-            ps.setString(4, alumno.getFechaNac().toString());
-            ps.setBoolean(5, alumno.isEstado());
+  public void guardarAlumno(Alumno alumno) {
+    try {
+      String sql = "INSERT INTO alumnos(dni,apellido,nombre,fecha_Nacimiento,estado) VALUES(?,?,?,?,?)";
+      PreparedStatement ps = connection.prepareStatement(sql);
+      ps.setInt(1, alumno.getDni());
+      ps.setString(2, alumno.getApellido());
+      ps.setString(3, alumno.getNombre());
+      ps.setString(4, alumno.getFechaNac().toString());
+      ps.setBoolean(5, alumno.isEstado());
 
-            ps.execute();
-        } catch (SQLException ex) {
-            System.err.println("Error en SQL");
-        }
+      ps.execute();
+      ps.close();
+    } catch (SQLException ex) {
+      System.err.println("Error en SQL");
+      System.out.println(ex);
     }
+  }
+
+  public void modificarAlumno(Alumno alumno) {
+    try {
+      String sql = "UPDATE alumnos SET dni = ? , apellido = ?, nombre = ?, fecha_Nacimiento = ?, estado = ? WHERE id_Alumno = ?";
+      PreparedStatement ps = connection.prepareStatement(sql);
+      ps.setInt(1, alumno.getDni());
+      ps.setString(2, alumno.getApellido());
+      ps.setString(3, alumno.getNombre());
+      ps.setString(4, alumno.getFechaNac().toString());
+      ps.setBoolean(5, alumno.isEstado());
+      ps.setInt(6, alumno.getIdAlumno());
+
+      int exito = ps.executeUpdate();
+
+      if (exito == 1) {
+        System.out.println("Modificado Exitosamente.");
+      } else {
+        System.out.println("El alumno no existe.");
+      }
+
+      ps.close();
+    } catch (SQLException ex) {
+      System.err.println("Error en SQL");
+      System.out.println(ex);
+    }
+  }
+
+  public void eliminarAlumno(int id) {
+    try {
+      String sql = "UPDATE alumnos SET estado = 0 WHERE id_Alumno = ? ";
+      PreparedStatement ps = connection.prepareStatement(sql);
+      ps.setInt(1, id);
+
+      int fila = ps.executeUpdate();
+
+      if (fila == 1) {
+        System.out.println("Eliminado Exitosamente.");
+      } else {
+        System.out.println("El alumno no existe.");
+      }
+
+      ps.close();
+    } catch (SQLException ex) {
+      System.err.println("Error en SQL");
+      System.out.println(ex);
+    }
+  }
 }
--- src/main/java/com/mycompany/proyectotrans/ProyectoTrans.java
+++ src/main/java/com/mycompany/proyectotrans/ProyectoTrans.java
@@ -1,26 +1,35 @@
 package com.mycompany.proyectotrans;
 
-import Entidades.Alumno;
 import Entidades.AlumnoData;
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 public class ProyectoTrans {
 
-    public static void main(String[] args) {
-        AlumnoData alumnoData = new AlumnoData();
+  public static void main(String[] args) {
+    AlumnoData alumnoData = new AlumnoData();
 
-        //INSERTAR ALUMNO
-//        Alumno alumno = new Alumno(41336432, "Fernandez", "Fernando", LocalDate.of(2000, Month.MARCH, 21), true);
-//        alumnoData.guardarAlumno(alumno);
-//LISTAR TODOS LOS ALUMNOS
+// INSERTAR ALUMNO
+//    Alumno alumnoNuevo = new Alumno(41336432, "Fernandez", "Fernando", LocalDate.of(2000, Month.MARCH, 21), true);
+//    alumnoData.guardarAlumno(alumnoNuevo);
+//
+// MODIFICAR ALUMNO
+//    Alumno alumnoBuscado = alumnoData.buscarAlumno(2);
+//    alumnoBuscado.setEstado(false);
+//    alumnoData.modificarAlumno(alumnoBuscado);
+//
+// LISTAR TODOS LOS ALUMNOS
 //        List<Alumno> alumnos;
 //        alumnos = alumnoData.listarAlumnos();
 //        System.out.println("Alumnos: " + alumnos);
-//BUSCAR POR ID
-        Alumno alumnoBuscado = alumnoData.buscarAlumno(2);
-        System.out.println(alumnoBuscado);
-
-    }
+//
+// BUSCAR POR ID
+//    Alumno alumnoBuscadoPorId = alumnoData.buscarAlumno(2);
+//    System.out.println(alumnoBuscadoPorId);
+//
+// BUSCAR POR DNI
+//    Alumno alumnoBuscadoPorDni = alumnoData.buscarAlumnoPorDni(41336432);
+//    System.out.println(alumnoBuscadoPorDni);
+//
+// ELIMINAR ALUMNO
+    alumnoData.eliminarAlumno(5);
+  }
 }
