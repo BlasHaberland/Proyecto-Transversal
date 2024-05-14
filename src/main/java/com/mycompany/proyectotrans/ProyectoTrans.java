e4d058c3e97d82f10178dcc387f7e9498e2f98ec
Blas Haberland <haberlandblas@gmail.com>
2024-05-14 10:17:54 -0300

    Entidad Alumno

---
+++ src/main/java/com/mycompany/proyectotrans/ProyectoTrans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectotrans;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProyectoTrans {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
